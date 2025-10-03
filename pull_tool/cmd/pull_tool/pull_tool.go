2b9ce6247faa913476d2f88cd3c15d51a48efcde
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-10-03 18:33:45 +0200

    Separate out "pull_tool" binary
    The logic for this is currently duplicated across img_tool and pull_tool, until the switch is complete.

---
+++ pull_tool/cmd/pull_tool/pull_tool.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/bazel-contrib/rules_img/pull_tool/cmd/downloadblob"
+	"github.com/bazel-contrib/rules_img/pull_tool/cmd/internal/pull"
+)
+
+const usage = `Usage: pull_tool [COMMAND] [ARGS...]
+
+Commands:
+  pull             pulls an image from a registry
+  download-blob    downloads a single blob from a registry`
+
+func Run(ctx context.Context, args []string) {
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+
+	command := args[1]
+	switch command {
+	case "pull":
+		pull.PullProcess(ctx, args[2:])
+	case "download-blob":
+		downloadblob.DownloadBlobProcess(ctx, args[2:])
+	default:
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	Run(ctx, os.Args)
+}
