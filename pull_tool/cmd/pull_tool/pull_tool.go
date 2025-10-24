2abbc030641834e2874bc98ce5c27aedf7c899b4
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-10-24 17:55:26 +0200

    pull: add tiny tool for creating basic directory structure

--- pull_tool/cmd/pull_tool/pull_tool.go
+++ pull_tool/cmd/pull_tool/pull_tool.go
@@ -13,7 +13,8 @@ const usage = `Usage: pull_tool [COMMAND] [ARGS...]
 
 Commands:
   pull             pulls an image from a registry
-  download-blob    downloads a single blob from a registry`
+  download-blob    downloads a single blob from a registry
+  mkdir [DIR]      creates a directory structure (including parents) `
 
 func Run(ctx context.Context, args []string) {
 	if len(args) < 2 {
@@ -27,6 +28,16 @@ func Run(ctx context.Context, args []string) {
 		pull.PullProcess(ctx, args[2:])
 	case "download-blob":
 		downloadblob.DownloadBlobProcess(ctx, args[2:])
+	case "mkdir":
+		if len(args) != 3 {
+			fmt.Fprintln(os.Stderr, "Usage: pull_tool mkdir [DIR]")
+			os.Exit(1)
+		}
+		dir := args[2]
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create directory %s: %v\n", dir, err)
+			os.Exit(1)
+		}
 	default:
 		fmt.Fprintln(os.Stderr, usage)
 		os.Exit(1)
