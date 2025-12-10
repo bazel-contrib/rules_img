4e5854c3c34d2626465a1a0803de0d548aa56625
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-12-10 12:58:33 +0100

    wip: extend credential sources
    Measured impact: The size of the pull tool binary goes from 7.4M to 10M.
    This should be reconsidered once we merge both tools into one again.

--- pull_tool/pkg/auth/registry/registry.go
+++ pull_tool/pkg/auth/registry/registry.go
@@ -1,9 +1,24 @@
 package registry
 
 import (
+	"io"
+	"sync"
+
+	ecr "github.com/awslabs/amazon-ecr-credential-helper/ecr-login"
+	"github.com/chrismellard/docker-credential-acr-env/pkg/credhelper"
 	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/authn/github"
 	"github.com/google/go-containerregistry/pkg/v1/google"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+var (
+	amazonKeychain = sync.OnceValue(func() authn.Keychain {
+		return authn.NewKeychainFromHelper(ecr.NewECRHelper(ecr.WithLogger(io.Discard)))
+	})
+	azureKeychain = sync.OnceValue(func() authn.Keychain {
+		return authn.NewKeychainFromHelper(credhelper.NewACRCredentialsHelper())
+	})
 )
 
 // WithAuthFromMultiKeychain returns a remote.Option that uses a MultiKeychain
@@ -13,6 +28,9 @@ func WithAuthFromMultiKeychain() remote.Option {
 	kc := authn.NewMultiKeychain(
 		authn.DefaultKeychain,
 		google.Keychain,
+		github.Keychain,
+		amazonKeychain(),
+		azureKeychain(),
 	)
 
 	return remote.WithAuthFromKeychain(kc)
