fabb86c5e85cab4cf85497b05e20d59d6b0f20e3
Matt Clarkson <mattyclarkson@gmail.com>
2026-04-16 12:25:43 +0100

    Add Bazel Credential Helper to `go-containerregistry` keychain (#456)
    Creates a wrapper around the current Bazel credential helper to conform
    to
    [`go-containerregistry@authn.Helper`](https://github.com/google/go-containerregistry/blob/e8813dd0a00e799459cae01d8a4659b9be2fd871/pkg/authn/keychain.go#L187C1-L194C2)
    interface so that it can be used with `authn.NewKeychainFromHelper`:
    
    ```go
    bazel := credential.New("bazel-credential-helper")
    docker := credential.ContainerRegistryHelper(bazel)
    keychain := authn. authn.NewKeychainFromHelper(docker)
    ```
    
    The URL is passed to the Bazel credential helper, headers are retrieved
    and the `Authorization` is read to provide the username/password from
    `Basic` _or_ the token from `Bearer`.
    
    The default keychain passed to `go-containerregistry` includes the Bazel
    credential helper when `IMG_CREDENTIAL_HELPER` is available in the
    environment.
    
    Things needing review:
    
    - Is `ContainerRegistryHelper` sensible naming?
    - There is a commit that copies the `ContainerRegistryHelper` to
    `pull_tool` as I didn't want to introduce a dependency to `img_tool`.
    What do we do about that?
    - There are unit tests but no integration tests. Guidance on if those
    are needed would be good to know. If so, where they go and how we expect
    them to be implemented would be good.
    - I haven't done exhaustive real-world testing of this. It'd be good to
    know what we need to land this.
    
    Fixes #453.

--- img_tool/pkg/auth/registry/registry.go
+++ img_tool/pkg/auth/registry/registry.go
@@ -1,6 +1,9 @@
 package registry
 
 import (
+	"os"
+
+	"github.com/bazel-contrib/rules_img/img_tool/pkg/auth/credential"
 	"github.com/malt3/go-containerregistry/pkg/authn"
 	"github.com/malt3/go-containerregistry/pkg/v1/google"
 	"github.com/malt3/go-containerregistry/pkg/v1/remote"
@@ -8,12 +11,23 @@ import (
 
 // WithAuthFromMultiKeychain returns a remote.Option that uses a MultiKeychain
 // combining the default keychain and the Google keychain for authentication.
-// WARNING: keep in sync with the same function in img_tool/pkg/auth/registry/registry.go.
+// If `IMG_CREDENTIAL_HELPER` is set in the environment, the Bazel credential helper will
+// contribute to the keychain.
+// WARNING: keep in sync with the same function in pull_tool/pkg/auth/registry/registry.go.
 func WithAuthFromMultiKeychain() remote.Option {
-	kc := authn.NewMultiKeychain(
+	keychains := []authn.Keychain{
 		authn.DefaultKeychain,
 		google.Keychain,
-	)
+	}
+
+	if value, ok := os.LookupEnv("IMG_CREDENTIAL_HELPER"); ok {
+		bazel := credential.New(value)
+		docker := credential.ContainerRegistryHelper(bazel)
+		keychain := authn.NewKeychainFromHelper(docker)
+		keychains = append(keychains, keychain)
+	}
+
+	kc := authn.NewMultiKeychain(keychains...)
 
 	return remote.WithAuthFromKeychain(kc)
 }
